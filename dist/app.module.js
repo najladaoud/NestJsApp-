60ae304b9046c439fa516d39b7974f583d7f5f1d
najla <najla.daoud@waialys.com>
2022-11-30 12:55:27 +0100

    supprimer node module

--- dist/app.module.js
+++ dist/app.module.js
@@ -8,15 +8,45 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.AppModule = void 0;
 const common_1 = require("@nestjs/common");
-const app_controller_1 = require("./app.controller");
-const app_service_1 = require("./app.service");
+const config_1 = require("@nestjs/config");
+const typeorm_1 = require("@nestjs/typeorm");
+const config_schema_1 = require("./config/config.schema");
+const auth_module_1 = require("./user/auth/auth.module");
+const user_module_1 = require("./user/user.module");
+const serve_static_1 = require("@nestjs/serve-static");
+const path_1 = require("path");
 let AppModule = class AppModule {
 };
 AppModule = __decorate([
     (0, common_1.Module)({
-        imports: [],
-        controllers: [app_controller_1.AppController],
-        providers: [app_service_1.AppService],
+        imports: [
+            config_1.ConfigModule.forRoot({
+                envFilePath: [`.env.stage.${process.env.STAGE}`],
+                validationSchema: config_schema_1.configValidationSchema,
+            }),
+            serve_static_1.ServeStaticModule.forRoot({
+                rootPath: (0, path_1.join)(__dirname, "..", "statics"),
+                renderPath: "./",
+            }),
+            typeorm_1.TypeOrmModule.forRootAsync({
+                imports: [config_1.ConfigModule],
+                inject: [config_1.ConfigService],
+                useFactory: async (configService) => ({
+                    type: "postgres",
+                    autoLoadEntities: true,
+                    synchronize: configService.get("SYNCHRONIZE"),
+                    host: configService.get("DB_HOST"),
+                    port: configService.get("DB_PORT"),
+                    username: configService.get("DB_USER"),
+                    password: configService.get("DB_PASSWORD"),
+                    database: configService.get("DB_DATABASE"),
+                    cache: true,
+                    logging: false,
+                }),
+            }),
+            auth_module_1.AuthModule,
+            user_module_1.UserModule,
+        ],
     })
 ], AppModule);
 exports.AppModule = AppModule;
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,38 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { configValidationSchema } from './config/config.schema';
+import { AuthModule } from './user/auth/auth.module';
+import { UserModule } from './user/user.module';
+import { ServeStaticModule } from "@nestjs/serve-static";
+import { join } from "path";
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [`.env.stage.${process.env.STAGE}`],
+      validationSchema: configValidationSchema,
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, "..", "statics"),
+      renderPath: "./",
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: "postgres",
+        autoLoadEntities: true,
+        synchronize: configService.get("SYNCHRONIZE"),
+        host: configService.get("DB_HOST"),
+        port: configService.get("DB_PORT"),
+        username: configService.get("DB_USER"),
+        password: configService.get("DB_PASSWORD"),
+        database: configService.get("DB_DATABASE"),
+        cache: true,
+        logging: false,
+      }),
+    }),
+    AuthModule,
+    UserModule,],
 })
 export class AppModule {}
